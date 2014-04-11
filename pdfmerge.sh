a347be2be148b842a7bdcbfb8ddc827fb811a540
John He <johnhe00@gmail.com>
2014-04-11 07:27:22 -0400

    Rewrite pdfmerge

--- pdfmerge.sh
+++ pdfmerge.sh
@@ -1,38 +1,23 @@
 #!/bin/bash
 
 function usage {
-  printf "Usage: pdfmerge <inputs *> <output>\n"
-}
-
-function error {
-  printf "$@" 1>&2
-  exit 1
-}
-
-function build {
-  if [ $# -lt 2 ]; then
-    error "$(usage)"
-  fi
-
-  local command="pdftk"
-  local output="${!#}"
-  local files="${@:1:$(($#-1))}"
-
-  for file in $files; do
-    local escaped=$(printf $file | sed 's/ /\\ /g')
-    if [ ! -f "$escaped" ]; then
-      error "$file: no such file or directory"
-    fi
-    command="$command $escaped"
-  done
-  printf "$command cat output $output"
-  return 0
+cat << EOF
+usage: pdfmerge <inputs> <output>
+EOF
 }
 
 function main {
-  IFS=$'\n'
-  local command="$(build $@)"
-  $command
+  # Last parameter
+  local output=${!#}
+  # Everything else
+  local inputs=${@:1:$(($#-1))}
+
+  if [ -z "$inputs" ]; then
+    usage
+    exit 1
+  fi
+
+  pdftk "${@:1:$(($#-1))}" cat output "$output"
 }
 
-main $@
+main "$@"
