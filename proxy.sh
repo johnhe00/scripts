0d0da06180cbd942eddd478214dd88211dbfe286
John He <johnhe00@gmail.com>
2015-11-05 15:53:58 -0500

    Abstract variables from proxy

--- proxy.sh
+++ proxy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-readonly APP_NAME="ssh-proxy"
-readonly CMD="ssh -D 12345 glacier"
+readonly SERVER="glacier"
+readonly PORT="12345"
+readonly APP_NAME="ssh-$SERVER-tunnel"
+readonly CMD="ssh -C -D $PORT $SERVER"
 
 source /home/johnhe00/scripts/app.sh
 main "$@"
